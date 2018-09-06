333c30fcb4a7f253119570ca11a825eac0a3c347
LiPengFei <124717880@qq.com>
2018-09-06 18:23:58 +0800

    # WARNING: head commit changed in the meantime
    创建工程目录结构

---
+++ xhpower-admin-dubbo/src/main/java/com/xhpower/xhpower_admin_App.java
@@ -0,0 +1,11 @@
+package com.xhpower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class xhpower_admin_App {
+	public static void main(String[] args) {
+		SpringApplication.run(xhpower_admin_App.class, args);
+	}
+}
